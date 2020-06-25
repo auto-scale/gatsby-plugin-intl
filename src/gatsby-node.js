2cabd622109d3a08b67ae5c6912cba7c687660a3
su- <aaaiiiuuu35@gmail.com>
2020-06-25 23:41:29 +0900

    Update gatsby-node.js

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -71,8 +71,9 @@ exports.onCreatePage = async ({ page, actions }, pluginOptions) => {
   }
 
   const generatePage = (routed, language) => {
-    // const canRedirect = !ignoreRedirectUrls.some(url => new RegExp(`^${url}`).test(page.path))
-    const canRedirect = true
+    const canRedirect = !ignoreRedirectUrls.some(url => new RegExp(`^${url}`).test(page.path))
+    console.log(page.path, canRedirect)
+    // const canRedirect = true
     const messages = getMessages(path, language)
     const newPath = routed ? `/${language}${page.path}` : page.path
     return {
